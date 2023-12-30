38c0a836da46f65f2be35be5b05417500d7ff487
Deepak Rai <deepakraii9696@gmail.com>
2023-12-30 23:18:46 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App text-cyan-700 text-center text-3xl font-bold">
       <h1>Helloooo</h1>
+      <Home />
     </div>
   );
 }
